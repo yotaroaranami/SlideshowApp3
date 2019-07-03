234b1ffb967889be1ef09aa291c40f2cc8676ee0
yotaroaranami <yotaro.aranami@i.softbank.jp>
2019-07-03 20:13:05 +0900

    Initial Commit

---
+++ SlideshowApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SlideshowApp
+//
+//  Created by Yotaro Aranami on 2019/07/03.
+//  Copyright © 2019 Yotaro Aranami. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
